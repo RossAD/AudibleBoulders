62b41c40c05d86baf86626fd3f48e882d9ba065a
Ross Davis <Ross.ad@gmail.com>
2016-03-12 08:53:07 -0800

    Create routes file

---
+++ src/server/config/helpers.js
@@ -0,0 +1,40 @@
+// var jwt = require('jwt-simple');
+
+
+module.exports = {
+  errorLogger: function (error, req, res, next) {
+    // log the error then send it to the next middleware in
+    console.error(error.stack);
+    next(error);
+  },
+  errorHandler: function (error, req, res, next) {
+    // send error message to client
+    // message for gracefull error handling on app
+    res.send(500, {error: error.message});
+  },
+
+  // Dummy function to test routes
+  testy: function (req, res, next) {
+    res.json("Route Succesful");
+  }
+
+  // Tolken handler, not sure if will use
+
+  // decode: function (req, res, next) {
+  //   var token = req.headers['x-access-token'];
+  //   var user;
+  //   if (!token) {
+  //     return res.send(403); // send forbidden if a token is not provided
+  //   }
+
+  //   try {
+  //     // decode token and attach user to the request
+  //     // for use inside our controllers
+  //     user = jwt.decode(token, 'secret');
+  //     req.user = user;
+  //     next();
+  //   } catch (error) {
+  //     return next(error);
+  //   }
+  // }
+};
--- src/server/server.js
+++ src/server/server.js
@@ -3,8 +3,8 @@ var app = express();
 var bodyparser = require('body-parser');
 var db = require('./db');
 
-app.use(bodyparser.urlencoded({extended : true}));
-app.use(bodyparser.json());
+// app.use(bodyparser.urlencoded({extended : true}));
+// app.use(bodyparser.json());
 
 var port = process.env.PORT || 8080;
 
@@ -15,7 +15,14 @@ var router = express.Router();
 //   res.json({message : 'Welcome to our Awesome server!'});
 // });
 
+// Routes for GitHelperServer
+
+require('./config/middleware.js')(app, express);
+require('./config/routes.js')(app, express);
+
 app.use('/api', router);
 
 app.listen(port);
 console.log('This is where it all begins... on port: ' + port);
+
+module.exports = app;
