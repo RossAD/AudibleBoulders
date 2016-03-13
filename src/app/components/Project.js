46d12fab25ed150161ca77df9788a9fdb6f19ff3
Albert Huynh <alberthuynh91@gmail.com>
2016-03-12 17:22:56 -0800

    Completed adding linter andreact router

---
+++ src/app/components/Project.js
@@ -0,0 +1,10 @@
+// modules/Repo.js
+import React from 'react';
+
+const Project = () => (
+  <div>
+    <h2>{this.props.params.repoName}</h2>
+  </div>
+);
+
+export default Project;
--- src/app/containers/App.js
+++ src/app/containers/App.js
@@ -1,24 +1,22 @@
-import React from 'react'
-import NavLink from '../components/NavLink'
-import { Link } from 'react-router'
+import React from 'react';
+import NavLink from '../components/NavLink';
+import Home from '../components/Home';
 
-export default React.createClass({
-  render() {
-    return (
-      <div>
-        <h1>GitSpy</h1>
-        <ul role="nav">
-          <li><NavLink to="/create">create</NavLink></li>
-          <li><NavLink to="/projects">projects</NavLink></li>
-          <li><NavLink to="/" onlyActiveOnIndex>Home</NavLink></li>
-        </ul>
+const App = (props) => (
+  <div>
+    <h1>GitSpy</h1>
+    <ul role="nav">
+      <li><NavLink to="/create">create</NavLink></li>
+      <li><NavLink to="/projects">projects</NavLink></li>
+      <li><NavLink to="/" onlyActiveOnIndex>Home</NavLink></li>
+    </ul>
+    {/* ... */}
+    {props.children || <Home />}
+  </div>
+);
 
-        {/* ... */}
-        {this.props.children || <Home/>}
+App.propTypes = {
+  children: React.PropTypes.node,
+};
 
-      </div>
-
-
-    )
-  }
-})
+export default App;
