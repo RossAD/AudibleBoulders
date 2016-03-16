4a517a01788b37f6e40c2bc03b77904d923a48e8
Albert Huynh <alberthuynh91@gmail.com>
2016-03-16 14:40:53 -0700

    Working commit, need to account for duplicates

--- src/server/request-handlers/dashboards.js
+++ src/server/request-handlers/dashboards.js
@@ -5,50 +5,48 @@ var db = require('../db');
 module.exports = {
   handlePost: function (req, res, next) {
     var users_id = req.body.users_id;
-    var dashboards_id = req.body.dashboard_id;
     var repo_link = req.body.repo_link;
 
-    var queryStr = "SELECT id FROM users_dashboards WHERE users_id='" + users_id.toString() + "' AND dashboards_id='" + dashboards_id.toString() + "'";
+    // var dashboards_id = req.body.dashboards_id;
+
+    var findDashboard = "SELECT * FROM dashboards WHERE repo_link='" + repo_link.toString() + "';";
 
     // Query for existing dashboard
-    db.query(queryStr, function (err, results) {
+    db.query(findDashboard, function (err, results) {
       if (err) {
         throw new Error(err);
       }
       if (results.length === 0) {
-        // Create new dashboard and associate user to dashboard
+        // Dashboard doesn't exist, create dashboard and associate user to the dashboard
         var createDashboard = "INSERT INTO dashboards (repo_link) VALUES ('" + repo_link + "');";
-        var associateUser = "INSERT INTO user_dashboards (users_id, dashboards_id, set_up, up_to_date) VALUES ('" + users_id.toString() + "', '" + dashboards_id.toString() + "', '0', '0');";
-
         db.query(createDashboard, function (err, results) {
           if (err) {
             throw new Error(err);
-          } else {
-            console.log('Successfully inserted into dashboard table');
-            db.query(associateUser, function (err, results) {
-              if (err) {
-                throw new Error(err);
-              } else {
-                console.log('Successfully inserted into users_dashboards table');
-                res.send(201);
-              }
-            });
           }
+          var dashboards_id = results.insertId;
+          var associateUser = "INSERT INTO user_dashboards (users_id, dashboards_id, set_up, up_to_date) VALUES ('" + users_id.toString() + "', '" + dashboards_id.toString() + "', '0', '0');";
+          db.query(associateUser, function (err, results) {
+            if (err) {
+              throw new Error(err);
+            } else {
+              console.log('Successfully inserted into users_dashboards table');
+              res.send(201);
+            }
+          });
         });
       } else {
-        // Add user to the dashboard
-        var insertQuery = "INSERT INTO user_dashboards (users_id, dashboards_id, set_up, up_to_date) VALUES ('" + users_id.toString() + "', '" + dashboards_id.toString() + "', '0', '0');";
-
-        db.query(insertQuery, function (err, results) {
+        // Dashboard does exist, associate user with dashboard
+        var dashboards_id = results[0].id;
+        var associateUser = "INSERT INTO users_dashboards (users_id, dashboards_id, set_up, up_to_date) VALUES ('" + users_id.toString() + "', '" + dashboards_id.toString() + "', '0', '0');";
+        db.query(associateUser, function (err, results) {
           if (err) {
             throw new Error(err);
-          }
-          if (results) {
+          } else {
             console.log('Successfully inserted into users_dashboards table');
             res.send(201);
           }
         });
       }
     });
-   }
+  }
 };
