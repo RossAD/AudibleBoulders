e3848bbb53474dac1cf999d509394b8d0a2e45e7
Albert Huynh <alberthuynh91@gmail.com>
2016-03-16 10:51:22 -0700

    Working on dashboard sql

--- src/server/request-handlers/dashboards.js
+++ src/server/request-handlers/dashboards.js
@@ -4,6 +4,51 @@ var db = require('../db');
 
 module.exports = {
   handlePost: function (req, res, next) {
+    var users_id = req.body.users_id;
+    var dashboards_id = req.body.dashboard_id;
+    var repo_link = req.body.repo_link;
 
-  }
+    var queryStr = "SELECT id FROM users_dashboards WHERE users_id='" + users_id.toString() + "' AND dashboards_id='" + dashboards_id.toString() + "'";
+
+    // Query for existing dashboard
+    db.query(queryStr, function (err, results) {
+      if (err) {
+        throw new Error(err);
+      }
+      if (results.length === 0) {
+        // Create new dashboard and associate user to dashboard
+        var createDashboard = "INSERT INTO dashboards (repo_link) VALUES ('" + repo_link + "');";
+        var associateUser = "INSERT INTO user_dashboards (users_id, dashboards_id, set_up, up_to_date) VALUES ('" + users_id.toString() + "', '" + dashboards_id.toString() + "', '0', '0');";
+
+        db.query(createDashboard, function (err, results) {
+          if (err) {
+            throw new Error(err);
+          } else {
+            console.log('Successfully inserted into dashboard table');
+            db.query(associateUser, function (err, results) {
+              if (err) {
+                throw new Error(err);
+              } else {
+                console.log('Successfully inserted into users_dashboards table');
+                res.send(201);
+              }
+            });
+          }
+        });
+      } else {
+        // Add user to the dashboard
+        var insertQuery = "INSERT INTO user_dashboards (users_id, dashboards_id, set_up, up_to_date) VALUES ('" + users_id.toString() + "', '" + dashboards_id.toString() + "', '0', '0');";
+
+        db.query(insertQuery, function (err, results) {
+          if (err) {
+            throw new Error(err);
+          }
+          if (results) {
+            console.log('Successfully inserted into users_dashboards table');
+            res.send(201);
+          }
+        });
+      }
+    });
+   }
 };
