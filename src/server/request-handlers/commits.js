7c33db2f22037508a1531ed470a1cb7e9ce9ea9c
Albert Huynh <alberthuynh91@gmail.com>
2016-03-17 14:27:41 -0700

    Implement POST api endpoint for commits

--- src/server/request-handlers/commits.js
+++ src/server/request-handlers/commits.js
@@ -1,10 +1,7 @@
+/*jshint loopfunc: true */
 "use strict";
 
 var db = require('../db');
-
-var createUsersDashboardsEntry = function() {
-  console.log('createUsersDashboardsEntry called');
-};
 
 module.exports = {
   handlePost: function (req, res, next) {
@@ -13,49 +10,60 @@ module.exports = {
     var last_pulled_commit = req.body.last_pulled_commit;
     var users_id = req.body.users_id;
 
+    // Find the user/dashboard association to get the id used to reference the diffs for that user
     var findUserDashboards = "SELECT * FROM users_dashboards WHERE users_id='" + users_id.toString() + "' AND dashboards_id='" + dashboards_id + "';";
     db.query(findUserDashboards, function (err, results) {
       if (err) {
         throw new Error(err);
       }
       if (results.length === 0) {
-        // Cannot find user/dashboard association
-        console.log('Did not find user/dash association');
+        return;
       } else {
-        // Found user/dash association
+        // Delete all existing diffs for the user for selected dashboard
         var users_dashboards_id = results[0].id;
         var deleteDiffs = "DELETE FROM diffs WHERE users_dashboards_id='" + users_dashboards_id + "';";
         db.query(deleteDiffs, function (err, results) {
           if (err) {
             throw new Error(err);
           }
-          console.log('Old diffs have been removed...');
+          // For each diff, create new diff for selected user/dashboard
           for (var i = 0; i < diffs.length; i++) {
-            var insertDiff = "INSERT INTO diffs (file, mod_type, commit_message, users_dashboards_id) VALUES ('" + diffs[i].file + "', '" + diffs[i].mod_type + "', '" + diffs[i].commit_message + "', '" + users_dashboards_id + "';";
-            // db.query(insertDiff, function (err, results) {
-            //   if (err) {
-            //     throw new Error(err);
-            //   }
-            //   console.log('Created a new diff');
-            // });
+            var insertDiff = "INSERT INTO diffs (file, mod_type, commit_message, users_dashboards_id) VALUES ('" + diffs[i].file.toString() + "', '" + diffs[i].mod_type.toString() + "', '" + diffs[i].commit_message.toString() + "', '" + users_dashboards_id.toString() + "');";
+            db.query(insertDiff, function (err, results) {
+              if (err) {
+                throw new Error(err);
+              }
+            });
           }
-          var checkLastCommit = "SELECT last_commit FROM dashboards WHERE id='" + dashboards_id.toString() + "';";
+          // Check dashboard's last commit against user's last pulled commit
+          // If the two commits are eq, set up_to_date field to true, else, set to false and update the last_pulled_commit in users_dashboards
+          var checkLastCommit = "SELECT * FROM dashboards WHERE id='" + dashboards_id.toString() + "';";
           db.query(checkLastCommit, function (err, results) {
             if (err) {
               throw new Error(err);
             }
             if (results[0].last_commit !== last_pulled_commit) {
-              // Update the 'up_to_date' record to false
+              console.log(results[0]);
+              console.log(last_pulled_commit);
+              var updateUptodateFalse = "UPDATE users_dashboards SET up_to_date='0', last_pulled_commit='" + last_pulled_commit + "' WHERE users_id='" + users_id.toString() + "' AND dashboards_id='" + dashboards_id.toString() + "';";
+              db.query(updateUptodateFalse, function (err, results) {
+                if (err) {
+                  throw new Error(err);
+                }
+                res.send(201);
+              });
             } else {
-              // Update the 'up_to_date' record to true
+              var updateUptodateTrue = "UPDATE users_dashboards SET up_to_date='1' WHERE users_id='" + users_id.toString() + "' AND dashboards_id='" + dashboards_id.toString() + "';";
+              db.query(updateUptodateTrue, function (err, results) {
+                if (err) {
+                  throw new Error(err);
+                }
+                res.send(201);
+              });
             }
           });
         });
       }
     });
-    // 1) find an entry in users_dashboards table given userid and dashid, return its record id in users_dashboards
-    // 2) delete all entries from diffs table where users_dashboards_id matches above
-    // 3) iterate through req.body.diffs, make a new entry in diffs table for each
-    // 4) query dashboards and check if last_commit is eq to last_pulled_commit, if not, see up_to_date for that user to false.
   }
 };
