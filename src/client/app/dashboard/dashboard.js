71684756c576e3518a6ef6538865927cf8eeb046
Albert Huynh <alberthuynh91@gmail.com>
2016-03-17 19:00:17 -0700

    Removed extraneous console logs

--- src/client/app/dashboard/dashboard.js
+++ src/client/app/dashboard/dashboard.js
@@ -1,6 +1,6 @@
 "use strict";
 angular.module('dashboard', [])
-.controller('DashboardController', function ($scope, $window, $location, $routeParams, RequestFactory) {
+.controller('DashboardController', function ($scope, $routeParams, RequestFactory) {
   $scope.dashboard = [];
 
   $scope.getDashboard = function () {
@@ -8,7 +8,6 @@ angular.module('dashboard', [])
     var repoName = $routeParams.repoName;
     RequestFactory.getDashboard(orgName, repoName)
     .then(function (data) {
-      console.log('the data: ', data);
       $scope.users = data.users;
       $scope.dashboard = data.dashboard;
     });
