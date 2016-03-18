7356fecb1132b54bd2184134b294ddfd065f292f
Albert Huynh <alberthuynh91@gmail.com>
2016-03-17 17:29:26 -0700

    Implement dashboard client view, added css

--- src/client/app/dashboard/dashboard.js
+++ src/client/app/dashboard/dashboard.js
@@ -1,5 +1,18 @@
 "use strict";
 angular.module('dashboard', [])
-.controller('DashboardController', function($scope, $window, RequestFactory){
-  console.log('DashboardController');
+.controller('DashboardController', function ($scope, $window, $location, $routeParams, RequestFactory) {
+  $scope.dashboard = [];
+
+  $scope.getDashboard = function () {
+    var orgName = $routeParams.orgName;
+    var repoName = $routeParams.repoName;
+    RequestFactory.getDashboard(orgName, repoName)
+    .then(function (data) {
+      console.log('the data: ', data);
+      $scope.users = data.users;
+      $scope.dashboard = data.dashboard;
+    });
+  };
+
+  $scope.getDashboard();
 });
