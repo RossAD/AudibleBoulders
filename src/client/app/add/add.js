de8d5da418c7df290cd27aaa308c7fdb2ae87e5b
Ross Davis <Ross.ad@gmail.com>
2016-03-19 17:39:02 -0700

    Add click reaction and repo added message  Changes to be committed: 	modified:   src/client/app/add/add.html 	modified:   src/client/app/add/add.js 	modified:   src/client/styles/style.css

--- src/client/app/add/add.js
+++ src/client/app/add/add.js
@@ -1,14 +1,40 @@
 "use strict";
 angular.module('add', [])
-.controller('AddController', ['$scope', '$window', 'RequestFactory', '$http',function($scope, $window, RequestFactory, $http){
+.controller('AddController', ['$scope', '$window', 'RequestFactory', '$http',function($scope, $window, RequestFactory, $http, $timeout){
   // Variable to hold returned subscriptions
+  $scope.repoAdded = false;
+  $scope.newRepo = '';
+  var timeOfLastAdd;
+
   $http({
     method: 'GET',
     url: '/api/subscriptions'
   }).then(function (res){
     $scope.subsc = res.data;
   });
+  // Add Repo Message
+  var addRepoMessage = function (repo) {
+    $scope.newRepo = repo;
+    $scope.repoAdded = true;
+  };
+
+  var expireRepoMessage = function (milliseconds) {
+    setTimeout(function() {
+      $scope.$apply(function() {
+        if ($scope.repoAdded === true && (Date.now() - timeOfLastAdd >= milliseconds)) {
+          $scope.repoAdded = false;
+        }
+      });
+    }, milliseconds);
+  };
+
   $scope.postDashboard = function (repoObject) {
     RequestFactory.postDashboard(repoObject);
+    timeOfLastAdd = Date.now();
+    addRepoMessage(repoObject.full_name);
+    expireRepoMessage(3000);
   };
+
+
+
 }]);
