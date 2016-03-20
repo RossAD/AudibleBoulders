582d0e18936e89b59eeab015262eb30cf5709be1
Ross Davis <Ross.ad@gmail.com>
2016-03-19 18:06:10 -0700

    removed unused $timeout call from add.js

--- src/client/app/add/add.js
+++ src/client/app/add/add.js
@@ -1,15 +1,12 @@
 "use strict";
 angular.module('add', [])
-.controller('AddController', ['$scope', '$window', 'RequestFactory', '$http',function($scope, $window, RequestFactory, $http, $timeout){
-  // Variable to hold returned subscriptions
-
+.controller('AddController', ['$scope', '$window', 'RequestFactory', '$http',function($scope, $window, RequestFactory, $http){
   $http({
     method: 'GET',
     url: '/api/subscriptions'
   }).then(function (res){
     $scope.subsc = res.data;
   });
-
   $scope.postDashboard = function (repoObject) {
     RequestFactory.postDashboard(repoObject);
   };
