9c873ad8682ea9bd407c763e781dd6fc7de29893
Ross Davis <Ross.ad@gmail.com>
2016-03-19 17:45:15 -0700

    Remove add repo message

--- src/client/app/add/add.js
+++ src/client/app/add/add.js
@@ -2,9 +2,6 @@
 angular.module('add', [])
 .controller('AddController', ['$scope', '$window', 'RequestFactory', '$http',function($scope, $window, RequestFactory, $http, $timeout){
   // Variable to hold returned subscriptions
-  $scope.repoAdded = false;
-  $scope.newRepo = '';
-  var timeOfLastAdd;
 
   $http({
     method: 'GET',
@@ -12,29 +9,8 @@ angular.module('add', [])
   }).then(function (res){
     $scope.subsc = res.data;
   });
-  // Add Repo Message
-  var addRepoMessage = function (repo) {
-    $scope.newRepo = repo;
-    $scope.repoAdded = true;
-  };
-
-  var expireRepoMessage = function (milliseconds) {
-    setTimeout(function() {
-      $scope.$apply(function() {
-        if ($scope.repoAdded === true && (Date.now() - timeOfLastAdd >= milliseconds)) {
-          $scope.repoAdded = false;
-        }
-      });
-    }, milliseconds);
-  };
 
   $scope.postDashboard = function (repoObject) {
     RequestFactory.postDashboard(repoObject);
-    timeOfLastAdd = Date.now();
-    addRepoMessage(repoObject.full_name);
-    expireRepoMessage(3000);
   };
-
-
-
 }]);
