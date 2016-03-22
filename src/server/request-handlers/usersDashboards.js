2a52a9d0e11e0ce7bb38cd41ac9045a52afd781b
Rhombus33 <diamond.d.wheeler@gmail.com>
2016-03-21 21:48:38 -0700

    Wrap delete handler in mysql pool

--- src/server/request-handlers/usersDashboards.js
+++ src/server/request-handlers/usersDashboards.js
@@ -1,7 +1,7 @@
 /*jshint loopfunc: true */
 "use strict";
 
-var db = require('../db');
+var pool = require('../db/index.js');
 
 module.exports = {
   handleDelete: function (req, res, next) {
@@ -10,61 +10,66 @@ module.exports = {
 
     // get users_id based on githubId
     var selectUserStr = "SELECT * FROM users WHERE github_id='" + githubId + "'";
-    db.query(selectUserStr, function (err, results) {
-      if (err) {
-        throw new Error(err);
-      } else {
-        var users_id = results[0].id;
-        var selectUserDashboardStr = "SELECT * from users_dashboards WHERE users_id= '" + users_id + "' AND dashboards_id= '" + dashboards_id + "'";
-        db.query(selectUserDashboardStr, function(err, results) {
-          if (err) {
-            throw new Error(err);
-          } else {
-            var users_dashboards_id = results[0].id;
-            var selectDiffsStr = "SELECT * from diffs WHERE users_dashboards_id= '" + users_dashboards_id + "'";
-            db.query(selectDiffsStr, function(err, results) {
-              if (err) {
-                throw new Error(err);
-              } else {
-                var diffs = results;
-                var deleted = 0;
+    pool.getConnection(function(err, connection){
+      if(err) {
+        throw err;
+      }
+      connection.query(selectUserStr, function (err, results) {
+        if (err) {
+          throw new Error(err);
+        } else {
+          var users_id = results[0].id;
+          var selectUserDashboardStr = "SELECT * from users_dashboards WHERE users_id= '" + users_id + "' AND dashboards_id= '" + dashboards_id + "'";
+          connection.query(selectUserDashboardStr, function(err, results) {
+            if (err) {
+              throw new Error(err);
+            } else {
+              var users_dashboards_id = results[0].id;
+              var selectDiffsStr = "SELECT * from diffs WHERE users_dashboards_id= '" + users_dashboards_id + "'";
+              connection.query(selectDiffsStr, function(err, results) {
+                if (err) {
+                  throw new Error(err);
+                } else {
+                  var diffs = results;
+                  var deleted = 0;
 
-                if (diffs.length > 0) {
-                  for (var i = 0; i < diffs.length; i++) {
-                    var deleteDiffsStr = "DELETE from diffs WHERE id= '" + results[i].id + "'";
-                    db.query(deleteDiffsStr, function(err, results) {
+                  if (diffs.length > 0) {
+                    for (var i = 0; i < diffs.length; i++) {
+                      var deleteDiffsStr = "DELETE from diffs WHERE id= '" + results[i].id + "'";
+                      connection.query(deleteDiffsStr, function(err, results) {
+                        if (err) {
+                          throw new Error(err);
+                        } else {
+                          deleted++;
+                          if (deleted === diffs.length) {
+                            var deleteUserDashboardStr = "DELETE from users_dashboards WHERE id= '" + users_dashboards_id + "'";
+                            connection.query(deleteUserDashboardStr, function(err, results) {
+                              if (err) {
+                                throw new Error(err);
+                              } else {
+                                res.json(results);
+                              }
+                            });
+                          }
+                        }
+                      });
+                    }
+                  } else {
+                    var deleteUserDashboardStr = "DELETE from users_dashboards WHERE id= '" + users_dashboards_id + "'";
+                    connection.query(deleteUserDashboardStr, function(err, results) {
                       if (err) {
                         throw new Error(err);
                       } else {
-                        deleted++;
-                        if (deleted === diffs.length) {
-                          var deleteUserDashboardStr = "DELETE from users_dashboards WHERE id= '" + users_dashboards_id + "'";
-                          db.query(deleteUserDashboardStr, function(err, results) {
-                            if (err) {
-                              throw new Error(err);
-                            } else {
-                              res.json(results);
-                            }
-                          });
-                        }
+                        res.json(results);
                       }
                     });
                   }
-                } else {
-                  var deleteUserDashboardStr = "DELETE from users_dashboards WHERE id= '" + users_dashboards_id + "'";
-                  db.query(deleteUserDashboardStr, function(err, results) {
-                    if (err) {
-                      throw new Error(err);
-                    } else {
-                      res.json(results);
-                    }
-                  });
                 }
-              }
-            });
-          }
-        });
-      }
+              });
+            }
+          });
+        }
+      });
     });
   }
 };
