c8cbddd437c7129e8c9549538e9dd528054f54dd
Ross Davis <Ross.ad@gmail.com>
2016-03-23 23:14:03 -0700

    Renamed files to fit refactor  Changes to be committed: 	modified:   src/server/config/helpers.js 	modified:   src/server/config/routes.js 	renamed:    src/server/query/github.js -> src/server/request-handlers/repos.js

--- src/server/config/routes.js
+++ src/server/config/routes.js
@@ -11,7 +11,7 @@ var dashboardsOrgRepo = require('../request-handlers/dashboards-org-repo');
 var setup = require('../request-handlers/setup');
 var commits = require('../request-handlers/commits');
 var usersDashboards = require('../request-handlers/usersDashboards');
-var github = require('../query/github.js');
+var repos = require('../request-handlers/repos.js');
 
 module.exports = function (app) {
   // 'helpers.testy' is a placeholder to test routing, replace with appropriate functions
@@ -30,7 +30,7 @@ module.exports = function (app) {
 
   // Get user GitHub Subscriptions
   app.get('/api/subscriptions/', users.userSub);
-  app.post('/api/repos/' , github.handlePost);
+  app.post('/api/repos/' , repos.handlePost);
 
   // Error handling
   app.use(helpers.errorLogger);
--- src/server/request-handlers/repos.js
+++ src/server/request-handlers/repos.js
@@ -2,13 +2,11 @@
 var helper = require('../config/helpers.js');
 var request = require('request');
 
-// Akshay Git Id 4003102
 module.exports = {
   handlePost : function(req, res, next) {
-    var gitID = req.cookies.gitHubId;
+    var gitID = req.cookies.githubId;
     helper.gitURL(gitID, req.body.link, function(nextPage){
       res.json(nextPage);
     });
   }
 };
-
