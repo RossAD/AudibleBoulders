8220de9363684f3ebec407e5494fdc5a9d2c3089
Alice Yu <yaliceme@gmail.com>
2016-03-23 22:49:34 -0700

    Fill in findOrCreate method for dashboards queries

--- src/server/queries/dashboards.js
+++ src/server/queries/dashboards.js
@@ -32,10 +32,40 @@ module.exports = {
       });
     });
   },
-  updateLastCommit: function (newSha1, newMsg, orgName, repoName, callback) {
+  updateLastCommit: function (orgName, repoName, newSha1, newMsg, callback) {
     // no return value
   },
   findOrCreate: function (orgName, repoName, callback) {
     // return id and a bool for whether it was a find or a create
+    // e.g. {dashboards_id: 1234, isNewDashboard: true}
+
+    // call getOne to see if dashboard already exists
+    module.exports.getOne(orgName, repoName, function (err, dashboardObject) {
+      if (err) {
+        callback(err, null);
+      } else if (dashboardObject) {
+        // dashboard DOES exist - pass its id to callback, with false bool for isNewDashboard
+        callback(null, {dashboards_id: dashboardObject.id, isNewDashboard: false});
+      } else {
+        // dashboard DOES NOT exist - create a new dashboard entry
+        pool.getConnection(function (err, connection) {
+          if (err) {
+            throw new Error(err);
+          }
+          // TODO: eventually, we will pass in branchName as an arg and use that, for now default to master
+          var branchName = "master";
+          var insertStr = "INSERT INTO dashboards (org_name, repo_name, branch_name) VALUES ('" + orgName + "', '" + repoName + "', '" + branchName + "');";
+          connection.query(insertStr, function (err, results) {
+            if (err) {
+              callback(err, null);
+            } else {
+              var dashboards_id = results.insertId;
+              callback(null, {dashboards_id: dashboards_id, isNewDashboard: true});
+            }
+            connection.release();
+          });
+        });
+      }
+    });
   }
 };
