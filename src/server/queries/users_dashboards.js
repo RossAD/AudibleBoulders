e4d561c9bd623f6684c7d22bad29268f65d89b00
Alice Yu <yaliceme@gmail.com>
2016-03-25 11:38:18 -0700

    Rename findOrCreate to associateUser in users_dashboards queries

--- src/server/queries/users_dashboards.js
+++ src/server/queries/users_dashboards.js
@@ -5,12 +5,12 @@ var pool = require('../db/index.js');
 module.exports = {
   // NOTE: by "return", we really mean "pass to callback as results arg"
 
-  findOrCreate: function (githubId, dashboardId, callback) {
-    // if create, initialize with set_up=0. otherwise do nothing
-    // no need to return anything (we don't think)
-  },
   getOne: function (githubId, dashboardId, callback) {
     // return matching users_dashboards record
+  },
+  associateUser: function (githubId, dashboardId, callback) {
+    // if create, initialize with set_up=0. otherwise do nothing
+    // no need to return anything (we don't think)
   },
   setUpTrue: function (signatureHash, callback) {
     // for that users_dashboards record, set set_up to 1
