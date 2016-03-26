087619e31a6aecea958a5af167a9483b864fbac4
Alice Yu <yaliceme@gmail.com>
2016-03-25 19:18:14 -0700

    Switch routes to use new request handlers

--- src/server/config/routes.js
+++ src/server/config/routes.js
@@ -4,18 +4,16 @@
 var helpers = require('./helpers.js');
 
 // request handlers
-var users = require('../request-handlers/users');
-var dashboards = require('../request-handlers/dashboards');
-var dashboardsGithubId = require('../request-handlers/dashboards-githubId');
-var dashboardsOrgRepo = require('../request-handlers/dashboards-org-repo');
-var setup = require('../request-handlers/setup');
-var commits = require('../request-handlers/commits');
-var usersDashboards = require('../request-handlers/usersDashboards');
-var repos = require('../request-handlers/repos.js');
+var users = require('../new-request-handlers/users');
+var dashboards = require('../new-request-handlers/dashboards');
+var dashboardsGithubId = require('../new-request-handlers/dashboards-githubId');
+var dashboardsOrgRepo = require('../new-request-handlers/dashboards-org-repo');
+var setup = require('../new-request-handlers/setup');
+var commits = require('../new-request-handlers/commits');
+var usersDashboards = require('../new-request-handlers/usersDashboards');
+var repos = require('../new-request-handlers/repos.js');
 
 module.exports = function (app) {
-  // 'helpers.testy' is a placeholder to test routing, replace with appropriate functions
-
   // Interact with dashboards
   app.post('/api/dashboards/', dashboards.handlePost);
   app.get('/api/dashboards/:githubId', dashboardsGithubId.handleGet);
