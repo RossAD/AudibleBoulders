193bb0bdc554577095cabdf885beea6647f81cd7
Rhombus33 <diamond.d.wheeler@gmail.com>
2016-03-26 12:01:59 -0700

    Refactor dashboards-githubId handler

--- src/server/new-request-handlers/dashboards-githubId.js
+++ src/server/new-request-handlers/dashboards-githubId.js
@@ -1,34 +1,12 @@
 "use strict";
 
-var pool = require('../db/index.js');
+var dashboards = require('../queries/dashboards.js');
 
 module.exports = {
   handleGet: function (req, res, next) {
     var githubId = req.params.githubId;
-
-    // get users_id based on githubId
-    var selectStr = "SELECT * FROM users WHERE github_id='" + githubId + "'";
-    pool.getConnection(function(err, connection){
-      if(err) {
-        throw err;
-      }
-      connection.query(selectStr, function (err, results) {
-        if (err) {
-          throw new Error(err);
-        } else {
-          var users_id = results[0].id;
-
-          // get dashboard details based on users_id, send as respons
-          var selectStr = "SELECT dashboards.id, repo_link, branch, org_name, repo_name, last_commit FROM users_dashboards INNER JOIN dashboards ON users_dashboards.dashboards_id=dashboards.id WHERE users_id='" + users_id + "'";
-          connection.query(selectStr, function(err, results) {
-            if (err) {
-              throw new Error(err);
-            } else {
-              res.json(results);
-            }
-          });
-        }
-      });
+    dashboards.getAllByGithubIdAsync(function(dashboards) {
+      res.json(dashboards);
     });
   }
 };
