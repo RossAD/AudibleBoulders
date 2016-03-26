9df25dd475a9a5bbf9061ae01e6a3ac98cb7733d
Alice Yu <yaliceme@gmail.com>
2016-03-26 16:48:28 -0700

    Implement new-request-handlers/dashboards-org-repo.js
    Complete, but still untested for now
    	modified:   src/server/new-request-handlers/dashboards-org-repo.js
    	modified:   src/server/queries/github.js

--- src/server/new-request-handlers/dashboards-org-repo.js
+++ src/server/new-request-handlers/dashboards-org-repo.js
@@ -1,8 +1,10 @@
 /*jshint loopfunc: true */
 "use strict";
 
-var request = require('request');
-var pool = require('../db/index.js');
+var dashboards = require('../queries/dashboards');
+var users = require('../queries/users');
+var diffs = require('../queries/diffs');
+var github = require('../queries/github');
 
 // This request handler takes the orgname and reponame for a given dashboard and returns everything
 // needed to render that dashboard page
@@ -36,17 +38,58 @@ var pool = require('../db/index.js');
 module.exports = {
   handleGet: function (req, res, next) {
     var responseObject = {dashboard: {}, users: []};
+
     var githubId = req.cookies.githubId;
     var orgName = req.params.orgName;
     var repoName = req.params.repoName;
 
-    // update last commit to what's current at github.com
-    // get dashboard info and attach it to responseObject
-    // get users info and store it in a temp array
-    // iterate through the array and get diffs info for each user
-    // push user info into users array
-    // send responseObject once all users are in
+    // commitUrl hardcoded for master branch for now
+    var commitUrl = 'https://api.github.com/repos/' + orgName + '/' + repoName + '/commits/master';
 
+    users.getOneAsync(githubId)
+      .then(function (user) {
+        var userToken = user.github_token;
+        return github.queryAsync(commitUrl, userToken);
+      })
+      .then(function (commit) {
+        var commitSha1 = commit.sha;
+        var commitMsg = commit.commit.message;
+        return dashboards.updateLastCommitAsync(orgName, repoName, commitSha1, commitMsg);
+      })
+      .then(function () {
+        return dashboards.getOneAsync(orgName, repoName);
+      })
+      .then(function (dashboard) {
+        responseObject.dashboard.id = dashboard.id;
+        responseObject.dashboard.last_commit_sha1 = dashboard.last_commit_sha1;
+        responseObject.dashboard.last_commit_msg = dashboard.last_commit_msg;
+      })
+      .then(function () {
+        return users.getDashboardUsersAsync(responseObject.dashboard.id);
+      })
+      .then(function (dashboardUsers) {
+        var usersWithSigHashes = dashboardUsers;
 
+        for (var i = 0; i < usersWithSigHashes.length; i++) {
+          var thisUser = usersWithSigHashes[i];
+
+          diffs.getAllAsync(thisUser.signature_hash)
+            .then(function (diffsArray) {
+              responseObject.users.push({
+                github_id: thisUser.github_id,
+                github_handle: thisUser.github_handle,
+                github_name: thisUser.github_name,
+                github_avatar: thisUser.github_avatar,
+                set_up: thisUser.set_up,
+                last_pulled_commit_sha1: thisUser.last_commit_sha1,
+                last_pulled_commit_msg: thisUser.last_pulled_commit_msg,
+                diffs: diffsArray
+              });
+              if (responseObject.users.length === usersWithSigHashes.length) {
+                res.json(responseObject);
+              }
+            });
+        }
+      });
   }
 };
--- src/server/queries/github.js
+++ src/server/queries/github.js
@@ -1,8 +1,25 @@
 "use strict";
 
+var request = require('request');
 var promise = require('bluebird');
 
 // NOTE: when using the methods in this module, append "Async" to the end of the method name
 var github = module.exports = promise.promisifyAll({
-
+  query: function (url, token, callback) {
+    var options = {
+      url: url,
+      headers: {
+        'User-Agent': 'GitSpy',
+        authorization: 'token ' + token,
+        'content-type': 'application/json'
+      }
+    };
+    request.get(options, function (err, response, body) {
+      if (err) {
+        callback(err, null);
+      } else {
+        callback(null, JSON.parse(body));
+      }
+    });
+  }
 });
