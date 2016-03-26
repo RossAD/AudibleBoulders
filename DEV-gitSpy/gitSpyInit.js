742c17f8bb287eea8ba219c27a792316f7c9b08b
Ross Davis <Ross.ad@gmail.com>
2016-03-26 15:25:03 -0700

    Modify gitSpyInit to add post-rewrite hook, also update gitspy.zip

--- DEV-gitSpy/gitSpyInit.js
+++ DEV-gitSpy/gitSpyInit.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var spawnSync = require('child_process').spawnSync;
 
+var postReWritePath = './.git/hooks/post-rewrite';
 var postCommitPath = './.git/hooks/post-commit';
 var runGitSpy = '#!/bin/sh\nnode gitSpy/gitSpy.js';
 
@@ -13,11 +14,13 @@ function createFile(path, content) {
 }
 
 function main() {
-  /** Create pre-merge and post-commit scripts **/
+  /** Create post-rewrite and post-commit scripts **/
   createFile(postCommitPath, runGitSpy);
+  createFile(postReWritePath, runGitSpy);
 
   /** Make files executable **/
   spawnSync('chmod', ['+x', '.git/hooks/post-commit']);
+  spawnSync('chmod', ['+x', '.git/hooks/post-rewrite']);
   console.log("GitSpy hooks added ʕ•ᴥ•ʔ");
 }
 main();
