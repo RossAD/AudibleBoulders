29b7ca3a592743da5af60e8a87f59c83f3974269
Alice Yu <yaliceme@gmail.com>
2016-03-26 15:14:49 -0700

    Stub a file for github queries

---
+++ src/server/queries/github.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var promise = require('bluebird');
+
+// NOTE: when using the methods in this module, append "Async" to the end of the method name
+var github = module.exports = promise.promisifyAll({
+
+});
