729a18d85eaa1a99bf3e43d9a0456f994615a3ac
Rhombus33 <diamond.d.wheeler@gmail.com>
2016-03-28 11:28:06 -0700

    Change variable names in commits handler

--- src/server/new-request-handlers/commits.js
+++ src/server/new-request-handlers/commits.js
@@ -6,20 +6,20 @@ var diffs = require('../queries/diffs.js');
 
 module.exports = {
   handlePost: function (req, res, next) {
-    var dashboards_signature_hash = req.body.dashboards_signature_hash;
-    var diffs = req.body.diffs;
-    var new_params = {
+    var signatureHash = req.body.dashboards_signature_hash;
+    var commitDiffs = req.body.diffs;
+    var newParams = {
       last_pulled_commit_sha1: req.body.last_pulled_commit_sha1,
       last_pulled_commit_msg: req.body.last_pulled_commit_msg,
       commit_branch: req.body.commit_branch
-    }
+    };
 
-    users_dashboards.updateOneAsync(dashboards_signature_hash, new_params)
+    users_dashboards.updateOneAsync(signatureHash, newParams)
       .then(function() {
-        return diffs.deleteAllAsync(dashboards_signature_hash);
+        return diffs.deleteAllAsync(signatureHash);
       })
       .then(function() {
-        return diffs.addAllAsync(dashboards_signature_hash, diffs);
+        return diffs.addAllAsync(signatureHash, commitDiffs);
       })
       .catch(function(e) {
         console.log("Error: ", e);
