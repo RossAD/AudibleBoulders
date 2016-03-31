cd78e41aee69fc63e1c76a9a4a91717d97fa20e1
Ross Davis <Ross.ad@gmail.com>
2016-03-30 19:50:10 -0700

    Minor tweaks to index.html and login.js

--- src/client/app/login/login.js
+++ src/client/app/login/login.js
@@ -1,6 +1,6 @@
 "use strict";
 angular.module('login', [])
-.controller('LoginController', function($scope, $cookies,$window, $location, RequestFactory){
+.controller('LoginController', function($scope, $cookies, $window, $location, RequestFactory){
   $scope.user = $cookies.get('githubName');
   $scope.isActivePage = function(viewLocation) {
     return viewLocation === $location.path();
