0f259dda237ba72256b530a9555f87a861b45153
Alice Yu <yaliceme@gmail.com>
2016-04-18 23:07:14 -0700

    Rename usrObj to userObject 	modified:   src/server/request-handlers/repos.js

--- src/server/request-handlers/repos.js
+++ src/server/request-handlers/repos.js
@@ -32,9 +32,9 @@ module.exports = {
   handlePost : function(req, res, next) {
     var gitID = req.cookies.githubId;
     users.getOneAsync(gitID)
-      .then(function(usrObj){
-        var token = usrObj.token;
-        return github.queryAsync(req.body.link, usrObj.github_token);
+      .then(function(userObject){
+        var token = userObject.token;
+        return github.queryAsync(req.body.link, userObject.github_token);
       })
       .then(function(nextPage){
         res.json(nextPage);
